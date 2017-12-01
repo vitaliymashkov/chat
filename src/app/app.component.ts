2da0b4b16af58e1502c16d9be3d75cd8df758d9b
Vitaliy Mashkov <vetalmm@gmail.com>
2017-12-01 12:08:13 +0200

    Init App

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './views/app.component.html'
+    styleUrls: ['./styles/app.component.css']
+})
+export class AppComponent {
+    name= '';
+}

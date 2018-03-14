85d7bdcf4589b2656c2774f29f384cc55e6a8432
Vitaliy Mashkov <vitaliy.mashkov@developex.com>
2018-03-14 11:06:36 +0200

    add version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {VERSION} from '../version';
 
 @Component({
     selector: 'my-app',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./styles/app.component.css']
 })
 export class AppComponent {
+    version= VERSION;
     name= '';
     count: number=0;
     increase() : void {
